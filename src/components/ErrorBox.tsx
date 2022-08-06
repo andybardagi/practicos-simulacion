1ff2bfa81a1ce1dca41fe57d10d780232bd9bbbd
andybardagi <andresbardagi2002@gmail.com>
2022-08-05 21:47:41 -0300

    Add yup validation

---
+++ src/components/ErrorBox.tsx
@@ -0,0 +1,24 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+export default function ErrorBox({ errorMsg }: { errorMsg: string[] }) {
+    if (errorMsg.length === 0) {
+        return null;
+    }
+
+    return (
+        <Box
+            bgColor="#ffa87c"
+            display={'grid'}
+            placeItems="center"
+            py={4}
+            px={8}
+            borderRadius={8}
+            fontWeight="bold"
+        >
+            {errorMsg.map((message) => (
+                <p> - {message}</p>
+            ))}
+        </Box>
+    );
+}
--- src/components/TP1/CombinedCongruent.tsx
+++ src/components/TP1/CombinedCongruent.tsx
@@ -1,4 +1,5 @@
 import {
+    Box,
     Button,
     Flex,
     Input,
@@ -6,6 +7,8 @@ import {
     InputLeftAddon,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { number, object } from 'yup';
+import ErrorBox from '../ErrorBox';
 
 export default function CombinedCongruent() {
     const [formValues, SetformValues] = useState({
@@ -13,6 +16,25 @@ export default function CombinedCongruent() {
         c: '0',
         m: '0',
         x0: '0',
+    });
+    const [error, setError] = useState({
+        error: false,
+        message: [],
+    });
+
+    const validationSchema = object().shape({
+        a: number()
+            .min(1, 'El valor de A no puede ser menor a 1')
+            .required('Debe ingresar un valor para a'),
+        c: number()
+            .min(1, 'El valor de C no puede ser menor a 1')
+            .required('Debe ingresar un valor para c'),
+        m: number()
+            .min(1, 'El valor de M no puede ser menor a 1')
+            .required('Debe ingresar un valor para m'),
+        x0: number()
+            .min(0, 'El valor de la semilla no puede ser negativo')
+            .required('Debe ingresar un valor para la semilla (x0)'),
     });
 
     const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,6 +45,29 @@ export default function CombinedCongruent() {
                     ? parseInt(e.target.value, 10).toString()
                     : '0',
         });
+    };
+
+    const handleGenerateClick = async (
+        e: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        setError({
+            error: false,
+            message: [],
+        })
+        validationSchema
+            .validate(formValues, { abortEarly: false })
+            .then(() => {
+                console.log('formValues', formValues);
+            })
+            .catch((err) => {
+                setError({
+                    error: true,
+                    message: err.inner.map(
+                        (innerError: { path: string; message: string }) =>
+                            innerError.message,
+                    ),
+                });
+            });
     };
 
     return (
@@ -67,8 +112,12 @@ export default function CombinedCongruent() {
                 </InputGroup>
             </Flex>
             <Flex direction={'row'} justifyContent="end" py={2}>
-                <Button colorScheme={'linkedin'}>Generar</Button>
+                <Button colorScheme={'linkedin'} onClick={handleGenerateClick}>
+                    Generar
+                </Button>
             </Flex>
+
+            < ErrorBox errorMsg={error.message}/>
         </>
     );
 }
