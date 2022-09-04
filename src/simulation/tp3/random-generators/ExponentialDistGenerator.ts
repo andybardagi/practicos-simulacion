5293fce1dbb66141938d1040e7b4fc5b7b23044e
andybardagi <andresbardagi2002@gmail.com>
2022-09-04 16:26:33 -0300

    Exponential random

---
+++ src/simulation/tp3/random-generators/ExponentialDistGenerator.ts
@@ -0,0 +1,14 @@
+import { BaseDistGenerator } from './BaseDistGenerator';
+
+export class ExponentialDistGenerator extends BaseDistGenerator {
+    private lambda: number;
+
+    constructor(lambda: number) {
+        super();
+        this.lambda = lambda;
+    }
+
+    generateRandom(): number {
+        return (1 / this.lambda) * Math.log(this.getUniformRandom());
+    }
+}
