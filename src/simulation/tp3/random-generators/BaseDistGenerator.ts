f2c0b696a6a54503cda54756bf14a41f8c5b4e61
andybardagi <andresbardagi2002@gmail.com>
2022-09-04 14:59:04 -0300

    Generate BaseDistribution class

---
+++ src/simulation/tp3/random-generators/BaseDistGenerator.ts
@@ -0,0 +1,27 @@
+import CombinedCongruentGenerator from '../../tp1/random-generators/CombinedCongruentGenerator';
+export abstract class BaseDistGenerator {
+    private uniformGenerator: CombinedCongruentGenerator;
+    private generation: number[] = [];
+
+    constructor() {
+        const now = new Date();
+        const initialSeed = now.getTime();
+        this.uniformGenerator = new CombinedCongruentGenerator(11, 7, 2 ** 61, initialSeed);
+    }
+
+    private getUniformRandom(): number {
+        return this.uniformGenerator.generateRandom();
+    }
+
+    abstract generateRandom(): number;
+
+    private generateDistribution(quantity: number) {
+        for (let i = 0; i < quantity; i++) {
+            this.generation.push(this.generateRandom());
+        }
+    }
+
+    public getGeneration(): number[] {
+        return this.generation;
+    }
+}
